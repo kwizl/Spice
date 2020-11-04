142b2f1d85398a3e177da3c1fe66e36969dd32b0
kwizl <karis.njoroge@gmail.com>
2020-11-04 09:44:01 -0800

    Add Category Index View

--- Spice/Areas/Admin/Controllers/CategoryController.cs
+++ Spice/Areas/Admin/Controllers/CategoryController.cs
@@ -3,15 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Spice.Data;
 
 namespace Spice.Areas.Admin.Controllers
 {
     [Area("Admin")]
     public class CategoryController : Controller
     {
-        public IActionResult Index()
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db)
         {
-            return View();
+            _db = db;
+        }
+
+        // GET Action Method
+        public async Task<IActionResult> Index()
+        {
+            var category = await _db.Category.ToListAsync();
+            return View(category);
         }
     }
 }
