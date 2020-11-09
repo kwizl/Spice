e5b6334fdf01a640bfe703f430c6c6bfe1deb86f
kwizl <karis.njoroge@gmail.com>
2020-11-09 05:24:42 -0800

    Create MenuItem View Model and the GET Action

---
+++ Spice/Data/Migrations/20201109122347_InitialMigration.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Spice.Data.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "MenuItem",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "MenuItem");
+        }
+    }
+}
--- Spice/Models/MenuItem.cs
+++ Spice/Models/MenuItem.cs
@@ -12,6 +12,8 @@ namespace Spice.Models
     {
         public int Id { get; set; }
 
+        public string Name { get; set; }
+
         public string Description { get; set; }
         
         public string Spicyness { get; set; }
